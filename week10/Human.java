da8be352564abf559bc2d4f632923327e6603366
North4655 <77388827+North4655@users.noreply.github.com>
2021-02-10 18:10:26 +0700

    week10

---
+++ week10/Human.java
@@ -0,0 +1,29 @@
+package week10;
+
+
+
+public class Human {
+       
+    
+    public void hit(Animal animal){
+          System.out.println(animal.roar());
+
+       }
+
+ public static void main (String[]args ) {
+     Animal dog = new Dog();//Question 1 
+    Human human = new Human() ;
+     System.out.println("the frist calling hit(Animal)");
+     human.hit(dog);//Question 2 
+
+
+    dog = new Cat();
+    System.out.println("the section calling hit(Anima)");
+    human.hit(dog);//Question 3
+
+ }
+
+
+
+
+}
